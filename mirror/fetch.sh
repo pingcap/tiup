5b94a01b370204ecd81165cd6babb99066a984b6
Lonng <heng@lonng.org>
2020-02-28 15:00:49 +0800

    fix fetch.sh
    Signed-off-by: Lonng <heng@lonng.org>

--- mirror/fetch.sh
+++ mirror/fetch.sh
@@ -4,7 +4,10 @@ for x in tidb tikv pd
 do
 	for i in {0..10}
 	do
-		wget  http://download.pingcap.org/$x-v3.0.$i-darwin-amd64.tar.gz
+		wget https://download.pingcap.org/$x-v3.0.$i-darwin-amd64.tar.gz
+		tar -xzf $x-v3.0.$i-darwin-amd64.tar.gz
+		tar -C $x-v3.0.$i-darwin-amd64 -czf $x-v3.0.$i-darwin-amd64.tar.gz bin/$x-server
+		rm -rf $x-v3.0.$i-darwin-amd64
 		sha1sum $x-v3.0.$i-darwin-amd64.tar.gz | awk '{print $1}' > $x-v3.0.$i-darwin-amd64.sha1
 	done
 done
@@ -12,11 +15,11 @@ done
 
 for i in {0..10}
 do
-	wget  http://download.pingcap.org/tidb-v3.0.$i-linux-amd64.tar.gz
+	wget  https://download.pingcap.org/tidb-v3.0.$i-linux-amd64.tar.gz
 	tar -xzf  tidb-v3.0.$i-linux-amd64.tar.gz
 	for x in tidb tikv pd
 	do
-		tar -czf  $x-v3.0.$i-linux-amd64.tar.gz tidb-v3.0.$i-linux-amd64/bin/$x-server
+		tar -C tidb-v3.0.$i-linux-amd64 -czf  $x-v3.0.$i-linux-amd64.tar.gz bin/$x-server
 		sha1sum $x-v3.0.$i-linux-amd64.tar.gz | awk '{print $1}' > $x-v3.0.$i-linux-amd64.sha1
 	done
 	rm -rf tidb-v3.0.$i-linux-amd64
