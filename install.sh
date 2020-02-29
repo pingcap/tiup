cce465fe73771cfe2d38d86722e136871e8485ce
Kolbe Kegel <kolbe@kolbekegel.com>
2020-02-28 20:37:33 -0800

    More improvements to install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,33 @@
 #!/bin/sh
 
 repo='https://tiup-mirrors.pingcap.com'
-case $(uname) in
+case $(uname -s) in
     Linux|linux) os=linux ;;
     Darwin|darwin) os=darwin ;;
-    *) os=unknown ;;
+    *) os= ;;
 esac
 
-if [ "$os" = unkonwn ]; then
-    echo "OS $(uname) not supported." >&2
+if [ -z "$os" ]; then
+    echo "OS $(uname -s) not supported." >&2
+    exit 1
+fi
+
+case $(uname -m) in
+    amd64|x86_64) arch=amd64 ;;
+    *) arch= ;;
+esac
+
+if [ -z "$arch" ]; then
+    echo "Architecture  $(uname -m) not supported." >&2
     exit 1
 fi
 
 if [ -z "$TIUP_HOME" ]; then
-    TIUP_HOME=${HOME}/.tiup
+    TIUP_HOME=$HOME/.tiup
 fi
 bin_dir=$TIUP_HOME/bin
 mkdir -p "$bin_dir"
-if ! curl "${repo}/tiup-${os}-amd64.tar.gz" | tar -zx -C "$bin_dir"; then
+if ! curl "$repo/tiup-$os-$arch.tar.gz" | tar -zx -C "$bin_dir"; then
     echo "Failed to download and/or extract tiup archive."
     exit 1
 fi
@@ -26,9 +36,9 @@ chmod 755 "$bin_dir/tiup"
 
 echo "$SHELL"
 case $SHELL in
-    *bash*) PROFILE=${HOME}/.bash_profile;;
-     *zsh*) PROFILE=${HOME}/.zshrc;;
-         *) PROFILE=${HOME}/.profile;;
+    *bash*) PROFILE=$HOME/.bash_profile;;
+     *zsh*) PROFILE=$HOME/.zshrc;;
+         *) PROFILE=$HOME/.profile;;
 esac
 
 bold=$(tput bold 2>/dev/null)
@@ -36,8 +46,8 @@ sgr0=$(tput sgr0 2>/dev/null)
 
 case :$PATH: in
     *:$bin_dir:*) : "PATH already contains $bin_dir" ;;
-    *) printf 'export PATH=%s:${PATH}\n' "$bin_dir" >> "$PROFILE"
-        echo "${PROFILE} has been modified to to add tiup to PATH"
+    *) printf 'export PATH=%s:$PATH\n' "$bin_dir" >> "$PROFILE"
+        echo "$PROFILE has been modified to to add tiup to PATH"
         echo "open a new terminal or ${bold}source ${PROFILE}${sgr0} to use it"
         ;;
 esac
