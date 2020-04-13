371e3054ffcb511403f016bf24e454a828460c63
Lonng <heng@lonng.org>
2020-04-13 22:14:16 +0800

    remove flag
    Signed-off-by: Lonng <heng@lonng.org>

--- components/book/main.go
+++ components/book/main.go
@@ -23,7 +23,6 @@ import (
 	"github.com/pingcap-incubator/tiup/pkg/localdata"
 	"github.com/pingcap-incubator/tiup/pkg/utils"
 	"github.com/skratchdot/open-golang/open"
-	"github.com/spf13/cobra"
 )
 
 func main() {
@@ -39,36 +38,14 @@ func execute() error {
 		return errors.New("component `book` cannot run in standalone mode")
 	}
 
-	var lang string
-	rootCmd := &cobra.Command{
-		Use: "tiup mirrors <target-dir>",
-		Example: `  tiup book --lang=en       # Open the English version of e-book <TiDB in Action>
-  tiup book --lang=zh_CN    # Open the Chinese version of e-book <TiDB in Action>`,
-		Short:        "Build a local mirrors and download all selected components",
-		SilenceUsage: true,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			if lang == "" {
-				return cmd.Help()
-			}
-
-			if lang != "zh_CN" {
-				return fmt.Errorf("language `%s` unspported (supported language: zh_CN)", lang)
-			}
-
-			port, err := utils.GetFreePort("", 39989)
-			if err != nil {
-				return errors.New("cannot find free port")
-			}
-
-			http.Handle("/", http.FileServer(http.Dir(filepath.Join(home, "_book"))))
-			if err := open.Run(fmt.Sprintf("http://localhost:%d", port)); err != nil {
-				return err
-			}
-			return http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
-		},
+	port, err := utils.GetFreePort("", 39989)
+	if err != nil {
+		return errors.New("cannot find free port")
 	}
 
-	rootCmd.Flags().StringVarP(&lang, "lang", "L", "", "Specify the `language` of e-book")
-
-	return rootCmd.Execute()
+	http.Handle("/", http.FileServer(http.Dir(filepath.Join(home, "_book"))))
+	if err := open.Run(fmt.Sprintf("http://localhost:%d", port)); err != nil {
+		return err
+	}
+	return http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
