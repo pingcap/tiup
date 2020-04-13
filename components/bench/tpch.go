b206f297dc45f9841995aa3010a07618a617fcc0
lucklove <gnu.crazier@gmail.com>
2020-04-13 22:53:38 +0800

    Add bench
    Signed-off-by: lucklove <gnu.crazier@gmail.com>

---
+++ components/bench/tpch.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"strings"
+
+	"github.com/pingcap/go-tpc/tpch"
+	"github.com/spf13/cobra"
+)
+
+var tpchConfig tpch.Config
+
+func executeTpch(action string, _ []string) {
+	openDB()
+	defer closeDB()
+
+	tpchConfig.DBName = dbName
+	tpchConfig.QueryNames = strings.Split(tpchConfig.RawQueries, ",")
+	w := tpch.NewWorkloader(globalDB, &tpchConfig)
+
+	timeoutCtx, cancel := context.WithTimeout(globalCtx, totalTime)
+	defer cancel()
+
+	executeWorkload(timeoutCtx, w, action)
+}
+
+func registerTpch(root *cobra.Command) {
+	cmd := &cobra.Command{
+		Use: "tpch",
+	}
+
+	cmd.PersistentFlags().StringVar(&tpchConfig.RawQueries,
+		"queries",
+		"q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,q16,q17,q18,q19,q20,q21,q22",
+		"All queries")
+
+	cmd.PersistentFlags().IntVar(&tpchConfig.ScaleFactor,
+		"sf",
+		0,
+		"scale factor")
+
+	cmd.PersistentFlags().BoolVar(&tpchConfig.EnableOutputCheck,
+		"check",
+		false,
+		"Check output data, only when the scale factor equals 1")
+
+	var cmdPrepare = &cobra.Command{
+		Use:   "prepare",
+		Short: "Prepare data for the workload",
+		Run: func(cmd *cobra.Command, args []string) {
+			executeTpch("prepare", args)
+		},
+	}
+
+	var cmdRun = &cobra.Command{
+		Use:   "run",
+		Short: "Run workload",
+		Run: func(cmd *cobra.Command, args []string) {
+			executeTpch("run", args)
+		},
+	}
+
+	var cmdCleanup = &cobra.Command{
+		Use:   "cleanup",
+		Short: "Cleanup data for the workload",
+		Run: func(cmd *cobra.Command, args []string) {
+			executeTpch("cleanup", args)
+		},
+	}
+
+	cmd.AddCommand(cmdRun, cmdPrepare, cmdCleanup)
+
+	root.AddCommand(cmd)
+}
--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/pingcap/check v0.0.0-20200212061837-5e12011dc712
 	github.com/pingcap/errors v0.11.5-0.20190809092503-95897b64e011
 	github.com/pingcap/failpoint v0.0.0-20200210140405-f8f9fb234798
+	github.com/pingcap/go-tpc v1.0.4-0.20200410025347-ee3e3247e342
 	github.com/pkg/errors v0.9.1
 	github.com/shirou/gopsutil v2.20.2+incompatible
 	github.com/spf13/cobra v0.0.6
