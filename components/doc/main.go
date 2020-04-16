9f4b2748e6bc86d9d8b2cbd34efe4fd2e9d2980b
SIGSEGV <gnu.crazier@gmail.com>
2020-04-16 21:40:04 +0800

    Switch doc to online mode (#164)
    Signed-off-by: lucklove <gnu.crazier@gmail.com>

--- components/doc/main.go
+++ components/doc/main.go
@@ -15,13 +15,8 @@ package main
 
 import (
 	"errors"
-	"fmt"
-	"net/http"
 	"os"
-	"path/filepath"
 
-	"github.com/pingcap-incubator/tiup/pkg/localdata"
-	"github.com/pingcap-incubator/tiup/pkg/utils"
 	"github.com/skratchdot/open-golang/open"
 	"github.com/spf13/cobra"
 )
@@ -40,29 +35,18 @@ func execute() error {
 		Short:        "The document abount TiDB",
 		SilenceUsage: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			home := os.Getenv(localdata.EnvNameComponentInstallDir)
-			if home == "" {
-				return errors.New("component `doc` cannot run in standalone mode")
-			}
-
-			port, err := utils.GetFreePort("", 39989)
-			if err != nil {
-				return errors.New("cannot find free port")
-			}
-
-			http.Handle("/", http.FileServer(http.Dir(filepath.Join(home, "dist"))))
 			if lang == "en" {
-				if err := open.Run(fmt.Sprintf("http://localhost:%d/docs/stable/", port)); err != nil {
+				if err := open.Run("https://pingcap.com/docs/stable"); err != nil {
 					return err
 				}
 			} else if lang == "cn" {
-				if err := open.Run(fmt.Sprintf("http://localhost:%d/docs-cn/stable/", port)); err != nil {
+				if err := open.Run("https://pingcap.com/docs-cn/stable"); err != nil {
 					return err
 				}
 			} else {
 				return errors.New("lang should be en or cn")
 			}
-			return http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+			return nil
 		},
 	}
 
