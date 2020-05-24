c41600b885be3d78451026f55343fbd6ba1fd078
Nick Cameron <nrc@ncameron.org>
2020-05-25 02:14:36 +1200

    manifest caching in local_manifests (#249)
    * Implement caching in lcoal_manifests
    
    Signed-off-by: Nick Cameron <nrc@ncameron.org>
    
    * Uncomment test and init cache map
    
    Co-authored-by: july2993 <july2993@gmail.com>

--- pkg/repository/v1manifest/local_manifests.go
+++ pkg/repository/v1manifest/local_manifests.go
@@ -14,7 +14,6 @@
 package v1manifest
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -22,27 +21,11 @@ import (
 	"path/filepath"
 	"strings"
 
+	cjson "github.com/gibson042/canonicaljson-go"
 	"github.com/pingcap-incubator/tiup/pkg/localdata"
 	"github.com/pingcap-incubator/tiup/pkg/repository/crypto"
 	"github.com/pingcap-incubator/tiup/pkg/utils"
 )
-
-// FsManifests represents a collection of v1 manifests on disk.
-// Invariant: any manifest written to disk should be valid, but may have expired. (It is also possible the manifest was
-// ok when written and has expired since).
-type FsManifests struct {
-	profile *localdata.Profile
-	keys    crypto.KeyStore
-}
-
-// FIXME implement garbage collection of old manifests
-
-// NewManifests creates a new FsManifests with local store at root.
-// There must exist the trusted root.json.
-// There must exists the trusted root.json.
-func NewManifests(profile *localdata.Profile) *FsManifests {
-	return &FsManifests{profile: profile}
-}
 
 // LocalManifests methods for accessing a store of manifests.
 type LocalManifests interface {
@@ -61,9 +44,36 @@ type LocalManifests interface {
 	InstallComponent(reader io.Reader, component, version string) error
 }
 
+// FsManifests represents a collection of v1 manifests on disk.
+// Invariant: any manifest written to disk should be valid, but may have expired. (It is also possible the manifest was
+// ok when written and has expired since).
+type FsManifests struct {
+	profile *localdata.Profile
+	keys    crypto.KeyStore
+	cache   map[string]string
+}
+
+// FIXME implement garbage collection of old manifests
+
+// NewManifests creates a new FsManifests with local store at root.
+// There must exist the trusted root.json.
+// There must exists the trusted root.json.
+func NewManifests(profile *localdata.Profile) *FsManifests {
+	return &FsManifests{profile: profile, cache: make(map[string]string)}
+}
+
 // SaveManifest implements LocalManifests.
 func (ms *FsManifests) SaveManifest(manifest *Manifest, filename string) error {
-	bytes, err := json.Marshal(manifest)
+	return ms.save(manifest, filename)
+}
+
+// SaveComponentManifest implements LocalManifests.
+func (ms *FsManifests) SaveComponentManifest(manifest *Manifest, filename string) error {
+	return ms.save(manifest, filename)
+}
+
+func (ms *FsManifests) save(manifest *Manifest, filename string) error {
+	bytes, err := cjson.Marshal(manifest)
 	if err != nil {
 		return err
 	}
@@ -73,63 +83,65 @@ func (ms *FsManifests) SaveManifest(manifest *Manifest, filename string) error {
 		return err
 	}
 
+	ms.cache[filename] = string(bytes)
 	return nil
-}
-
-// SaveComponentManifest implements LocalManifests.
-func (ms *FsManifests) SaveComponentManifest(manifest *Manifest, filename string) error {
-	bytes, err := json.Marshal(manifest)
-	if err != nil {
-		return err
-	}
-
-	return ioutil.WriteFile(filepath.Join(ms.profile.Root(), filename), bytes, 0644)
 }
 
 // LoadManifest implements LocalManifests.
 func (ms *FsManifests) LoadManifest(role ValidManifest) (bool, error) {
-	file, err := ms.load(role.Filename())
-	if err != nil {
+	filename := role.Filename()
+	manifest, err := ms.load(filename)
+	if err != nil || manifest == "" {
 		return false, err
 	}
-	if file == nil {
-		return false, nil
-	}
-	defer file.Close()
 
-	_, err = ReadManifest(file, role, nil)
-	return true, err
+	_, err = ReadManifest(strings.NewReader(manifest), role, nil)
+	if err != nil {
+		return true, err
+	}
+
+	ms.cache[filename] = manifest
+	return true, nil
 }
 
 // LoadComponentManifest implements LocalManifests.
 func (ms *FsManifests) LoadComponentManifest(index *Index, filename string) (*Component, error) {
-	file, err := ms.load(filename)
+	manifest, err := ms.load(filename)
+	if err != nil || manifest == "" {
+		return nil, err
+	}
+
+	component := new(Component)
+	_, err = ReadComponentManifest(strings.NewReader(manifest), component, index)
 	if err != nil {
 		return nil, err
 	}
-	if file == nil {
-		return nil, nil
-	}
-	defer file.Close()
 
-	com := new(Component)
-	_, err = ReadComponentManifest(file, com, index)
-	return com, err
+	ms.cache[filename] = manifest
+	return component, nil
 }
 
 // load return the file for the manifest from disk.
-// The returned file is not nil if the file do not exists.
-func (ms *FsManifests) load(filename string) (file *os.File, err error) {
-	fullPath := filepath.Join(ms.profile.Root(), filename)
-	file, err = os.Open(fullPath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return nil, nil
-		}
-		return nil, err
+// The returned string is empty if the file does not exist.
+func (ms *FsManifests) load(filename string) (string, error) {
+	str, cached := ms.cache[filename]
+	if cached {
+		return str, nil
 	}
 
-	return
+	fullPath := filepath.Join(ms.profile.Root(), filename)
+	file, err := os.Open(fullPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return "", nil
+		}
+		return "", err
+	}
+	defer file.Close()
+
+	builder := strings.Builder{}
+	io.Copy(&builder, file)
+	return builder.String(), nil
 }
 
 // Keys implements LocalManifests.
