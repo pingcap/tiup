bf989f654d9b2f11f34d1a34dc97306f468b722b
birdstorm <samuelwyf@hotmail.com>
2020-05-28 16:29:24 +0800

    increase wait time

--- components/playground/main.go
+++ components/playground/main.go
@@ -448,7 +448,7 @@ func bootCluster(options *bootOptions) error {
 		}
 
 		if lastErr == nil {
-			time.Sleep(10 * time.Second)
+			time.Sleep(30 * time.Second)
 			for _, flash := range flashs {
 				if err := flash.Start(ctx, v0manifest.Version(options.version), pathMap["tiflash"]); err != nil {
 					lastErr = err
