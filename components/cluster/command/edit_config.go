10a8d7772d9a2b3c5699808cb01c5e41752d62d4
july2993 <july2993@gmail.com>
2020-07-21 09:58:00 +0800

    Add help msg of EDITOR (#614)

--- components/cluster/command/edit_config.go
+++ components/cluster/command/edit_config.go
@@ -35,7 +35,7 @@ import (
 func newEditConfigCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "edit-config <cluster-name>",
-		Short: "Edit TiDB cluster config",
+		Short: "Edit TiDB cluster config.\nWill use editor from environment variable `EDITOR`, default use vi",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if len(args) != 1 {
 				return cmd.Help()
