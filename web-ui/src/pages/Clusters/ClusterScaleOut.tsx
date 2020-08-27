9394152256db78401bd4e6e9e2317544c4b03ecc
baurine <2008.hbl@gmail.com>
2020-08-27 16:51:32 +0800

    extract CompsManager

--- web-ui/src/pages/Clusters/ClusterScaleOut.tsx
+++ web-ui/src/pages/Clusters/ClusterScaleOut.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import { Root } from '../../components/Root'
 import { useParams } from 'react-router-dom'
+import CompsManager from '../Deployment/CompsManager'
 
 export default function ClusterScaleOutPage() {
   const { clusterName } = useParams()
 
-  return <Root>ClusterScaleOutPage : {clusterName}</Root>
+  return <CompsManager clusterName={clusterName} forScaleOut={true} />
 }
