d4ee68dd672c9ee78d0a1a5fcac61208162d2f87
baurine <2008.hbl@gmail.com>
2020-08-28 18:44:36 +0800

    use hashrouter

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
 import MachinesPage from './pages/Machines'
 import DeploymentPage from './pages/Deployment'
