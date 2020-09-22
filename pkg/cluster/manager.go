81ed482528b33b69ce8eff0778fec7f0e873310f
lucklove <gnu.crazier@gmail.com>
2020-09-22 19:12:25 +0800

    Fix type cast
    Signed-off-by: lucklove <gnu.crazier@gmail.com>

--- pkg/cluster/manager.go
+++ pkg/cluster/manager.go
@@ -597,13 +597,15 @@ func (m *Manager) Display(clusterName string, opt operator.Options) error {
 	cliutil.PrintTable(clusterTable, true)
 	fmt.Printf("Total nodes: %d\n", len(clusterTable)-1)
 
-	// Check if TiKV's label set correctly
-	kvs := topo.(*spec.Specification).TiKVServers
-	pdClient := api.NewPDClient(pdList, 10*time.Second, tlsCfg)
-	if lbs, err := pdClient.GetLocationLabels(); err != nil {
-		color.Yellow("\nWARN: get location labels from pd failed: %v", err)
-	} else if err := spec.CheckTiKVLocationLabels(lbs, kvs); err != nil {
-		color.Yellow("\nWARN: there is something wrong with TiKV labels, which may cause data losing:\n%v", err)
+	if topo, ok := topo.(*spec.Specification); ok {
+		// Check if TiKV's label set correctly
+		kvs := topo.TiKVServers
+		pdClient := api.NewPDClient(pdList, 10*time.Second, tlsCfg)
+		if lbs, err := pdClient.GetLocationLabels(); err != nil {
+			color.Yellow("\nWARN: get location labels from pd failed: %v", err)
+		} else if err := spec.CheckTiKVLocationLabels(lbs, kvs); err != nil {
+			color.Yellow("\nWARN: there is something wrong with TiKV labels, which may cause data losing:\n%v", err)
+		}
 	}
 
 	return nil
@@ -1070,10 +1072,10 @@ func (m *Manager) Deploy(
 		base.GlobalOptions.SSHType = sshType
 	}
 
-	if !opt.NoLabels {
+	if topo, ok := topo.(*spec.Specification); ok && !opt.NoLabels {
 		// Check if TiKV's label set correctly
-		lbs := topo.(*spec.Specification).LocationLabels()
-		kvs := topo.(*spec.Specification).TiKVServers
+		lbs := topo.LocationLabels()
+		kvs := topo.TiKVServers
 		if err := spec.CheckTiKVLocationLabels(lbs, kvs); err != nil {
 			return perrs.Errorf("check TiKV label failed, please fix that before continue:\n%s", err)
 		}
