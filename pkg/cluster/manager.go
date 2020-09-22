789765e50ac76fffd72cacb41ef8de94523e8b79
lucklove <gnu.crazier@gmail.com>
2020-09-22 18:45:14 +0800

    Adjust output
    Signed-off-by: lucklove <gnu.crazier@gmail.com>

--- pkg/cluster/manager.go
+++ pkg/cluster/manager.go
@@ -598,13 +598,11 @@ func (m *Manager) Display(clusterName string, opt operator.Options) error {
 	fmt.Printf("Total nodes: %d\n", len(clusterTable)-1)
 
 	// Check if TiKV's label set correctly
-	pdClient := api.NewPDClient(pdList, 10*time.Second, tlsCfg)
-	lbs, err := pdClient.GetLocationLabels()
-	if err != nil {
-		return err
-	}
 	kvs := topo.(*spec.Specification).TiKVServers
-	if err := spec.CheckTiKVLocationLabels(lbs, kvs); err != nil {
+	pdClient := api.NewPDClient(pdList, 10*time.Second, tlsCfg)
+	if lbs, err := pdClient.GetLocationLabels(); err != nil {
+		color.Yellow("\nWARN: get location labels from pd failed: %v", err)
+	} else if err := spec.CheckTiKVLocationLabels(lbs, kvs); err != nil {
 		color.Yellow("\nWARN: there is something wrong with TiKV labels, which may cause data losing:\n%v", err)
 	}
 
