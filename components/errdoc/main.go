0a59cb58ad49fc2d10b19e5d971e73f845e4f188
Lonng <heng@lonng.org>
2020-10-27 18:43:14 +0800

    fix imports
    Signed-off-by: Lonng <heng@lonng.org>

--- components/errdoc/main.go
+++ components/errdoc/main.go
@@ -21,11 +21,10 @@ import (
 	"sort"
 	"strings"
 
-	"github.com/blevesearch/bleve/search/query"
-
 	"github.com/BurntSushi/toml"
 	"github.com/blevesearch/bleve"
 	_ "github.com/blevesearch/bleve/index/store/goleveldb"
+	"github.com/blevesearch/bleve/search/query"
 	"github.com/pingcap/tiup/components/errdoc/spec"
 	"github.com/pingcap/tiup/pkg/localdata"
 	"github.com/spf13/cobra"
