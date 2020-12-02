7e0bcb79fbe73cc2d350aab26a6201e1c8d29be3
9547 <nivefive9547@gmail.com>
2020-12-02 08:08:57 +0800

    feat(cluster/prune): updateMeta and reload config after destroy tombstone

--- components/cluster/command/prune.go
+++ components/cluster/command/prune.go
@@ -14,15 +14,6 @@
 package command
 
 import (
-	"fmt"
-
-	"github.com/fatih/color"
-	perrs "github.com/pingcap/errors"
-	"github.com/pingcap/tiup/pkg/cliutil"
-	operator "github.com/pingcap/tiup/pkg/cluster/operation"
-	"github.com/pingcap/tiup/pkg/cluster/spec"
-	"github.com/pingcap/tiup/pkg/cluster/task"
-	"github.com/pingcap/tiup/pkg/logger/log"
 	"github.com/spf13/cobra"
 )
 
@@ -37,68 +28,9 @@ func newPruneCmd() *cobra.Command {
 
 			clusterName := args[0]
 
-			metadata, err := spec.ClusterMetadata(clusterName)
-			if err != nil {
-				return err
-			}
-
-			return destroyTombstoneIfNeed(clusterName, metadata, gOpt, skipConfirm)
+			return manager.DestroyTombstone(clusterName, gOpt, skipConfirm)
 		},
 	}
 
 	return cmd
-}
-
-func destroyTombstoneIfNeed(clusterName string, metadata *spec.ClusterMeta, opt operator.Options, skipConfirm bool) error {
-	topo := metadata.Topology
-
-	if !operator.NeedCheckTombstone(topo) {
-		return nil
-	}
-
-	tlsCfg, err := topo.TLSConfig(tidbSpec.Path(clusterName, spec.TLSCertKeyDir))
-	if err != nil {
-		return perrs.AddStack(err)
-	}
-
-	ctx := task.NewContext()
-	err = ctx.SetSSHKeySet(spec.ClusterPath(clusterName, "ssh", "id_rsa"),
-		spec.ClusterPath(clusterName, "ssh", "id_rsa.pub"))
-	if err != nil {
-		return perrs.AddStack(err)
-	}
-
-	err = ctx.SetClusterSSH(topo, metadata.User, gOpt.SSHTimeout, gOpt.SSHType, topo.BaseTopo().GlobalOptions.SSHType)
-	if err != nil {
-		return perrs.AddStack(err)
-	}
-
-	nodes, err := operator.DestroyTombstone(ctx, topo, true /* returnNodesOnly */, opt, tlsCfg)
-	if err != nil {
-		return perrs.AddStack(err)
-	}
-
-	if len(nodes) == 0 {
-		return nil
-	}
-
-	if !skipConfirm {
-		err = cliutil.PromptForConfirmOrAbortError(
-			color.HiYellowString(fmt.Sprintf("Will destroy these nodes: %v\nDo you confirm this action? [y/N]:", nodes)),
-		)
-		if err != nil {
-			return err
-		}
-	}
-
-	log.Infof("Start destroy Tombstone nodes: %v ...", nodes)
-
-	_, err = operator.DestroyTombstone(ctx, topo, false /* returnNodesOnly */, opt, tlsCfg)
-	if err != nil {
-		return perrs.AddStack(err)
-	}
-
-	log.Infof("Destroy success")
-
-	return spec.SaveClusterMeta(clusterName, metadata)
 }
