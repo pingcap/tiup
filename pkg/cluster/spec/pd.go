dcd2aa3d805786207ffd12ae518f6effa1f8f2ee
9547 <nivefive9547@gmail.com>
2020-12-13 00:04:39 +0800

    enhance(cluster/pd): pd.Status check by itself

--- pkg/cluster/spec/pd.go
+++ pkg/cluster/spec/pd.go
@@ -18,7 +18,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/pingcap/errors"
@@ -52,30 +51,19 @@ type PDSpec struct {
 }
 
 // Status queries current status of the instance
-func (s PDSpec) Status(tlsCfg *tls.Config, pdList ...string) string {
-	curAddr := fmt.Sprintf("%s:%d", s.Host, s.ClientPort)
-	curPdAPI := api.NewPDClient([]string{curAddr}, statusQueryTimeout, tlsCfg)
-	allPdAPI := api.NewPDClient(pdList, statusQueryTimeout, tlsCfg)
+func (s PDSpec) Status(tlsCfg *tls.Config, _ ...string) string {
+	addr := fmt.Sprintf("%s:%d", s.Host, s.ClientPort)
+	pc := api.NewPDClient([]string{addr}, statusQueryTimeout, tlsCfg)
 	suffix := ""
 
-	// find dashboard node
-	dashboardAddr, _ := allPdAPI.GetDashboardAddress()
-	if strings.HasPrefix(dashboardAddr, "http") {
-		r := strings.NewReplacer("http://", "", "https://", "")
-		dashboardAddr = r.Replace(dashboardAddr)
-	}
-	if dashboardAddr == curAddr {
-		suffix = "|UI"
-	}
-
 	// check health
-	err := curPdAPI.CheckHealth()
+	err := pc.CheckHealth()
 	if err != nil {
 		return "Down" + suffix
 	}
 
 	// find leader node
-	leader, err := curPdAPI.GetLeader()
+	leader, err := pc.GetLeader()
 	if err != nil {
 		return "ERR" + suffix
 	}
