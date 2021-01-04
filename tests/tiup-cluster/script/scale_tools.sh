76f3f496b05c1488158e46d000bab19da32f5ecd
9547 <nivefive9547@gmail.com>
2021-01-05 01:10:26 +0800

    fix(cluster/spec): missing {alertmanager,grafana}s

--- tests/tiup-cluster/script/scale_tools.sh
+++ tests/tiup-cluster/script/scale_tools.sh
@@ -25,6 +25,10 @@ function scale_tools() {
     tiup-cluster $client exec $name -N n1 --command "grep magic-string-for-test /home/tidb/deploy/prometheus-9090/conf/tidb.rules.yml"
     tiup-cluster $client exec $name -N n1 --command "grep magic-string-for-test /home/tidb/deploy/grafana-3000/dashboards/tidb.json"
     tiup-cluster $client exec $name -N n1 --command "grep magic-string-for-test /home/tidb/deploy/alertmanager-9093/conf/alertmanager.yml"
+    tiup-cluster $client exec $name -N n1 --command "grep alertmanagers /home/tidb/deploy/prometheus-9090/conf/prometheus.yml"
+    for item in pump drainer tidb tikv pd tiflash grafana node_exporter blackbox_exporter; do
+        tiup-cluster $client exec $name -N n1 --command "grep group: '$item' /home/tidb/deploy/prometheus-9090/conf/prometheus.yml"
+    done
 
     tiup-cluster $client list | grep "$name"
 
