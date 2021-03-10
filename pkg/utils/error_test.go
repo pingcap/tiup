b61b5ea7b8aabfdc23bdf18b9e7b0f6e02722d03
Allen Zhong <zhongbenli@pingcap.com>
2021-03-10 19:23:06 +0800

    utils: update test

--- pkg/utils/error_test.go
+++ pkg/utils/error_test.go
@@ -16,7 +16,6 @@ package utils
 import (
 	"errors"
 
-	"github.com/pingcap/check"
 	. "github.com/pingcap/check"
 )
 
@@ -32,40 +31,40 @@ func (s *errSuite) TestHashValidationErr(c *C) {
 		actual: "hash222",
 	}
 	// identical errors are equal
-	c.Assert(errors.Is(err0, err0), check.IsTrue)
-	c.Assert(errors.Is(ErrValidateChecksum, ErrValidateChecksum), check.IsTrue)
-	c.Assert(errors.Is(ErrValidateChecksum, &HashValidationErr{}), check.IsTrue)
-	c.Assert(errors.Is(&HashValidationErr{}, ErrValidateChecksum), check.IsTrue)
+	c.Assert(errors.Is(err0, err0), IsTrue)
+	c.Assert(errors.Is(ErrValidateChecksum, ErrValidateChecksum), IsTrue)
+	c.Assert(errors.Is(ErrValidateChecksum, &HashValidationErr{}), IsTrue)
+	c.Assert(errors.Is(&HashValidationErr{}, ErrValidateChecksum), IsTrue)
 	// not equal for different error types
-	c.Assert(errors.Is(err0, errors.New("")), check.IsFalse)
+	c.Assert(errors.Is(err0, errors.New("")), IsFalse)
 	// default Value matches any error
-	c.Assert(errors.Is(err0, ErrValidateChecksum), check.IsTrue)
+	c.Assert(errors.Is(err0, ErrValidateChecksum), IsTrue)
 	// error with values are not matching default ones
-	c.Assert(errors.Is(ErrValidateChecksum, err0), check.IsFalse)
+	c.Assert(errors.Is(ErrValidateChecksum, err0), IsFalse)
 
 	err1 := &HashValidationErr{
 		cipher: "sha256",
 		expect: "hash111",
 		actual: "hash222",
 	}
-	c.Assert(errors.Is(err1, ErrValidateChecksum), check.IsTrue)
+	c.Assert(errors.Is(err1, ErrValidateChecksum), IsTrue)
 	// errors with same values are equal
-	c.Assert(errors.Is(err0, err1), check.IsTrue)
-	c.Assert(errors.Is(err1, err0), check.IsTrue)
+	c.Assert(errors.Is(err0, err1), IsTrue)
+	c.Assert(errors.Is(err1, err0), IsTrue)
 	// errors with different ciphers are not equal
 	err1.cipher = "sha512"
-	c.Assert(errors.Is(err0, err1), check.IsFalse)
-	c.Assert(errors.Is(err1, err0), check.IsFalse)
+	c.Assert(errors.Is(err0, err1), IsFalse)
+	c.Assert(errors.Is(err1, err0), IsFalse)
 	// errors with different expected hashes are not equal
 	err1.cipher = err0.cipher
-	c.Assert(errors.Is(err0, err1), check.IsTrue)
+	c.Assert(errors.Is(err0, err1), IsTrue)
 	err1.expect = "hash1112"
-	c.Assert(errors.Is(err0, err1), check.IsFalse)
-	c.Assert(errors.Is(err1, err0), check.IsFalse)
+	c.Assert(errors.Is(err0, err1), IsFalse)
+	c.Assert(errors.Is(err1, err0), IsFalse)
 	// errors with different actual hashes are not equal
 	err1.expect = err0.expect
-	c.Assert(errors.Is(err0, err1), check.IsTrue)
+	c.Assert(errors.Is(err0, err1), IsTrue)
 	err1.actual = "hash2223"
-	c.Assert(errors.Is(err0, err1), check.IsFalse)
-	c.Assert(errors.Is(err1, err0), check.IsFalse)
+	c.Assert(errors.Is(err0, err1), IsFalse)
+	c.Assert(errors.Is(err1, err0), IsFalse)
 }
