85c8c4394d80c66fe2bf9658ae19d2ea80e1ddfd
Allen Zhong <zhongbenli@pingcap.com>
2021-04-09 15:50:33 +0800

    ci: bump tidb versions in test scripts (#1289)

--- tests/tiup-cluster/test_scale_core_tls.sh
+++ tests/tiup-cluster/test_scale_core_tls.sh
@@ -4,5 +4,5 @@ set -eu
 
 source script/scale_core.sh
 
-echo "test scaling of core components in cluster for verision v4.0.4 w/ TLS, via easy ssh"
-scale_core v4.0.4 true false
+echo "test scaling of core components in cluster for verision v4.0.12 w/ TLS, via easy ssh"
+scale_core v4.0.12 true false
