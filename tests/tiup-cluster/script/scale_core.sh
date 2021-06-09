94b01547f55b4609853a3e298bd2a106adc08da5
9547 <nivefive9547@gmail.com>
2021-06-09 11:34:29 +0800

    tests/cluster: --native-ssh is deprecated, use --ssh=system (#1418)

--- tests/tiup-cluster/script/scale_core.sh
+++ tests/tiup-cluster/script/scale_core.sh
@@ -11,7 +11,7 @@ function scale_core() {
 
     client=""
     if [ $native_ssh == true ]; then
-        client="--native-ssh"
+        client="--ssh=system"
     fi
 
     name="test_scale_core_$RANDOM"
