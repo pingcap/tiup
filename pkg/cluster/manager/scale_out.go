c89c78482e9c625dae77eed8ae768717f56765d4
9547 <nivefive9547@gmail.com>
2021-06-12 16:50:04 +0800

    cluster/scale_out: fillHostArch before validate
    Signed-off-by: 9547 <nivefive9547@gmail.com>

--- pkg/cluster/manager/scale_out.go
+++ pkg/cluster/manager/scale_out.go
@@ -84,6 +84,18 @@ func (m *Manager) ScaleOut(
 		return err
 	}
 
+	var sshConnProps *tui.SSHConnectionProps = &tui.SSHConnectionProps{}
+	if gOpt.SSHType != executor.SSHTypeNone {
+		var err error
+		if sshConnProps, err = tui.ReadIdentityFileOrPassword(opt.IdentityFile, opt.UsePassword); err != nil {
+			return err
+		}
+	}
+
+	if err := m.fillHostArch(sshConnProps, newPart, &gOpt, opt.User); err != nil {
+		return err
+	}
+
 	// Abort scale out operation if the merged topology is invalid
 	mergedTopo := topo.MergeTopo(newPart)
 	if err := mergedTopo.Validate(); err != nil {
@@ -136,18 +148,6 @@ func (m *Manager) ScaleOut(
 		if err := m.confirmTopology(name, base.Version, newPart, patchedComponents); err != nil {
 			return err
 		}
-	}
-
-	var sshConnProps *tui.SSHConnectionProps = &tui.SSHConnectionProps{}
-	if gOpt.SSHType != executor.SSHTypeNone {
-		var err error
-		if sshConnProps, err = tui.ReadIdentityFileOrPassword(opt.IdentityFile, opt.UsePassword); err != nil {
-			return err
-		}
-	}
-
-	if err := m.fillHostArch(sshConnProps, newPart, &gOpt, opt.User); err != nil {
-		return err
 	}
 
 	// Build the scale out tasks
