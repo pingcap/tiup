1d98db52c13d02a3da040e02655bc8c352301bb2
9547 <nivefive9547@gmail.com>
2021-07-01 11:55:26 +0800

    tests/cluster: trap bash exit  (#1462)

--- tests/tiup-cluster/script/util.sh
+++ tests/tiup-cluster/script/util.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 set -eu
+set -eE -o functrace
+
+failure() {
+    local lineno=$2
+    local fn=$3
+    local exitstatus=$4
+    local msg=$5
+    local lineno_fns=${1% 0}
+    if [[ "$lineno_fns" != "0" ]] ; then
+        lineno="${lineno} ${lineno_fns}"
+    fi
+    echo "${BASH_SOURCE[1]}:${fn}[${lineno}] Failed with status ${exitstatus}: $msg"
+}
+trap 'failure "${BASH_LINENO[*]}" "$LINENO" "${FUNCNAME[*]:-script}" "$?" "$BASH_COMMAND"' ERR
 
 # instance_num <name> <use-native-ssh>
 # get the instance number of the cluster
