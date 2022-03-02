75787b01d6749f087d020fafc96b2311f6e46c4d
nexustar <inexustar@gmail.com>
2022-03-02 14:37:54 +0800

    cluster: change initial_commit_ts to initial-commit-ts

--- pkg/cluster/ansible/service.go
+++ pkg/cluster/ansible/service.go
@@ -197,7 +197,7 @@ func parseDirs(ctx context.Context, user string, ins spec.InstanceSpec, sshTimeo
 				tsArg := strings.Split(line, " ")[4] // 4 whitespaces ahead
 				tmpTs, _ := strconv.Atoi(strings.TrimPrefix(tsArg, "--initial-commit-ts="))
 				newIns.Config = make(map[string]interface{})
-				newIns.Config["initial_commit_ts"] = int64(tmpTs)
+				newIns.Config["initial-commit-ts"] = int64(tmpTs)
 			}
 		}
 		return newIns, nil
