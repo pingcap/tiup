bdc10b9b8d044a1ed0b1e5f4ea6855be4211f01f
Neil Shen <overvenus@gmail.com>
2022-04-12 13:54:34 +0800

    cluster: enable data-dir for versions above v5.0.3 (#1838)

--- pkg/cluster/template/scripts/cdc.go
+++ pkg/cluster/template/scripts/cdc.go
@@ -124,26 +124,24 @@ func (c *CDCScript) AppendEndpoints(ends ...*PDScript) *CDCScript {
 
 // PatchByVersion update fields by cluster version
 func (c *CDCScript) PatchByVersion(clusterVersion, dataDir string) *CDCScript {
-	// for those version, cdc does not support --data-dir
-	ignore := map[string]struct{}{
-		"v5.0.0-rc":    {},
-		"v5.1.0-alpha": {},
-		"v5.2.0-alpha": {},
-	}
-
 	// config support since v4.0.13, ignore v5.0.0-rc
 	// the same to data-dir, but we treat it as --sort-dir
 	if semver.Compare(clusterVersion, "v4.0.13") >= 0 && clusterVersion != "v5.0.0-rc" {
 		c = c.WithConfigFileEnabled().WithDataDir(dataDir)
 	}
 
-	// cdc support --data-dir since v4.0.14 and v5.0.3, but not the ignore above
+	// cdc support --data-dir since v4.0.14 and v5.0.3
 	if semver.Major(clusterVersion) == "v4" && semver.Compare(clusterVersion, "v4.0.14") >= 0 {
 		c = c.WithDataDirEnabled()
 	}
 
-	if semver.Major(clusterVersion) == "v5" && semver.Compare(clusterVersion, "v5.0.3") >= 0 {
-		if _, ok := ignore[clusterVersion]; !ok {
+	// for those version above v5.0.3, cdc does not support --data-dir
+	ignoreVersion := map[string]struct{}{
+		"v5.1.0-alpha": {},
+		"v5.2.0-alpha": {},
+	}
+	if semver.Compare(clusterVersion, "v5.0.3") >= 0 {
+		if _, ok := ignoreVersion[clusterVersion]; !ok {
 			c = c.WithDataDirEnabled()
 		}
 	}
