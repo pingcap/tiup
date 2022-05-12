4a7acba1e9f00e3497bbc91708da32fa14215dd6
nexustar <inexustar@gmail.com>
2022-05-12 11:40:40 +0800

    why it not work

--- tests/tiup-playground/test_playground.sh
+++ tests/tiup-playground/test_playground.sh
@@ -102,8 +102,8 @@ killall -2 tiup-playground.test || killall -2 tiup-playground
 sleep 60
 
 # test restart with same data
-tiup-playground v6.0.0 > $outfile 2>&1 &
-
+# tiup-playground v6.0.0 > $outfile 2>&1 &
+timeout 200 tiup-playground v6.0.0 2>&1 
 # wait $outfile generated
 sleep 3
 
