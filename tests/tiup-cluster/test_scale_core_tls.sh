f062b88a7554e0705a6731883bc093de3058d5a7
nexustar <inexustar@gmail.com>
2022-05-12 19:58:35 +0800

    test: update tidb version (#1847)

--- tests/tiup-cluster/test_scale_core_tls.sh
+++ tests/tiup-cluster/test_scale_core_tls.sh
@@ -5,4 +5,4 @@ set -eu
 source script/scale_core.sh
 
 echo "test scaling of core components in cluster for version v5.3.0 w/ TLS, via easy ssh"
-scale_core v5.3.0 true false
+scale_core v4.0.12 true false
