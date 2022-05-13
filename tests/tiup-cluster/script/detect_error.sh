631f2fb2a906a71aa8d4a541ee2bc03674d79bca
nexustar <inexustar@gmail.com>
2022-05-13 17:39:17 +0800

    add stderr log detect

--- tests/tiup-cluster/script/detect_error.sh
+++ tests/tiup-cluster/script/detect_error.sh
@@ -3,7 +3,13 @@ set -eu
 
 err_num=$(find $1 -name "*.log" -exec grep "\[ERROR\]" {} \; | wc -l)
 if [ ${err_num} != "0" ]; then
-  echo ${err_num}
+  echo "detect ${err_num} [ERROR] log"
+  exit 1
+fi
+
+err_num=$(find $1 -name "*stderr.log" -exec cat {} \; | wc -l)
+if [ ${err_num} != "0" ]; then
+  echo "detect ${err_num} stderr log"
   exit 1
 fi
 
