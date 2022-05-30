5a5d2801bbc1a3c6b53908ed41f83eb398662577
nexustar <inexustar@gmail.com>
2022-05-30 16:38:51 +0800

    update

--- components/cluster/command/edit_config.go
+++ components/cluster/command/edit_config.go
@@ -23,7 +23,7 @@ func newEditConfigCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "edit-config <cluster-name>",
 		Short: "Edit TiDB cluster config",
-		Long:  "Edit TiDB cluster config.\nWill use editor from environment variable `EDITOR`, default use vi",
+		Long:  "Edit TiDB cluster config. Will use editor from environment variable `EDITOR`, default use vi",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if len(args) != 1 {
 				return cmd.Help()
