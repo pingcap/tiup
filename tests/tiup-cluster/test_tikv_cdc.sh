68a405441cfff9318f71b717a60522156b2c0258
pingyu <yuping@pingcap.com>
2022-09-07 10:48:49 +0800

    remove staging
    Signed-off-by: pingyu <yuping@pingcap.com>

--- tests/tiup-cluster/test_tikv_cdc.sh
+++ tests/tiup-cluster/test_tikv_cdc.sh
@@ -7,14 +7,14 @@ source script/tikv_cdc.sh
 
 ###############################################
 echo "Test common cases for TiKV-CDC"
-cmd_subtest --version 6.2.0 --topo tikv_cdc --staging
+cmd_subtest --version 6.2.0 --topo tikv_cdc
 
 ###############################################
 # TODO: test tls cases after TiKV-CDC support TLS.
 
 ###############################################
 echo "Test specified cases for TiKV-CDC"
-tikv_cdc_test --version 6.2.0 --topo tikv_cdc --staging --tikv-cdc-patch 1.0.0-alpha
+tikv_cdc_test --version 6.2.0 --topo tikv_cdc --tikv-cdc-patch 1.0.0
 
 ###############################################
 # NOTE: As TiKV-CDC always use the latest version, the upgrade does NOT cover the upgrade of TiKV-CDC itself.
@@ -23,4 +23,4 @@ tikv_cdc_test --version 6.2.0 --topo tikv_cdc --staging --tikv-cdc-patch 1.0.0-a
 
 ###############################################
 echo "Test scale in/out for TiKV-CDC" 
-tikv_cdc_scale_test --version "6.2.0" --topo tikv_cdc --staging
+tikv_cdc_scale_test --version "6.2.0" --topo tikv_cdc
