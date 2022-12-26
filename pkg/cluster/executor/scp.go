d119e8ba393587cc798c71b5b4c370157b44e28f
nexustar <inexustar@gmail.com>
2022-12-27 00:23:03 +0800

    allow download directory when use builtin scp

--- pkg/cluster/executor/scp.go
+++ pkg/cluster/executor/scp.go
@@ -23,7 +23,6 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/pingcap/tiup/pkg/utils"
 	"golang.org/x/crypto/ssh"
 )
 
@@ -41,62 +40,6 @@ func ScpDownload(session *ssh.Session, client *ssh.Client, src, dst string, limi
 		return err
 	}
 
-	copyF := func() error {
-		// parse SCP command
-		line, _, err := bufr.ReadLine()
-		if err != nil {
-			return err
-		}
-		if line[0] != byte('C') {
-			return fmt.Errorf("incorrect scp command '%b', should be 'C'", line[0])
-		}
-
-		mode, err := strconv.ParseUint(string(line[1:5]), 0, 32)
-		if err != nil {
-			return fmt.Errorf("error parsing file mode; %s", err)
-		}
-
-		// prepare dst file
-		targetPath := filepath.Dir(dst)
-		if err := utils.CreateDir(targetPath); err != nil {
-			return err
-		}
-		targetFile, err := os.OpenFile(dst, os.O_RDWR|os.O_CREATE|os.O_TRUNC, fs.FileMode(mode))
-		if err != nil {
-			return err
-		}
-		defer targetFile.Close()
-
-		size, err := strconv.Atoi(strings.Fields(string(line))[1])
-		if err != nil {
-			return err
-		}
-
-		if err := ack(w); err != nil {
-			return err
-		}
-
-		// transferring data
-		n, err := io.CopyN(targetFile, bufr, int64(size))
-		if err != nil {
-			return err
-		}
-		if n < int64(size) {
-			return fmt.Errorf("error downloading via scp, file size mismatch")
-		}
-		if err := targetFile.Sync(); err != nil {
-			return err
-		}
-
-		return ack(w)
-	}
-
-	copyErrC := make(chan error, 1)
-	go func() {
-		defer w.Close()
-		copyErrC <- copyF()
-	}()
-
 	remoteArgs := make([]string, 0)
 	if compress {
 		remoteArgs = append(remoteArgs, "-C")
@@ -104,7 +47,7 @@ func ScpDownload(session *ssh.Session, client *ssh.Client, src, dst string, limi
 	if limit > 0 {
 		remoteArgs = append(remoteArgs, fmt.Sprintf("-l %d", limit))
 	}
-	remoteCmd := fmt.Sprintf("scp %s -f %s", strings.Join(remoteArgs, " "), src)
+	remoteCmd := fmt.Sprintf("scp -r %s -f %s", strings.Join(remoteArgs, " "), src)
 
 	err = session.Start(remoteCmd)
 	if err != nil {
@@ -114,10 +57,76 @@ func ScpDownload(session *ssh.Session, client *ssh.Client, src, dst string, limi
 		return err
 	}
 
-	err = <-copyErrC
-	if err != nil {
-		return err
+	wd := dst
+	for {
+		// parse scp command
+		line, err := bufr.ReadString('\n')
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		switch line[0] {
+		// ignore ACK from server
+		case '\x00':
+			line = line[1:]
+		case '\x01':
+			return fmt.Errorf("scp warning: %s", line[1:])
+		case '\x02':
+			return fmt.Errorf("scp error: %s", line[1:])
+		}
+
+		switch line[0] {
+		case 'C':
+			mode, size, name, err := parseLine(line)
+			if err != nil {
+				return err
+			}
+
+			targetFile, err := os.OpenFile(filepath.Join(wd, name), os.O_RDWR|os.O_CREATE|os.O_TRUNC, mode|0700)
+			if err != nil {
+				return err
+			}
+			defer targetFile.Close()
+			if err := ack(w); err != nil {
+				return err
+			}
+
+			// transferring data
+			n, err := io.CopyN(targetFile, bufr, size)
+			if err != nil {
+				return err
+			}
+			if n < size {
+				return fmt.Errorf("error downloading via scp, file size mismatch")
+			}
+			if err := targetFile.Sync(); err != nil {
+				return err
+			}
+		case 'D':
+			mode, _, name, err := parseLine(line)
+			if err != nil {
+				return err
+			}
+
+			wd = filepath.Join(wd, name)
+			err = os.MkdirAll(wd, mode)
+			if err != nil {
+				return err
+			}
+		case 'E':
+			wd = filepath.Dir(wd)
+		default:
+			return fmt.Errorf("incorrect scp command '%b', should be 'C', 'D' or 'E'", line[0])
+		}
+
+		err = ack(w)
+		if err != nil {
+			return err
+		}
 	}
+
 	return session.Wait()
 }
 
@@ -131,4 +140,26 @@ func ack(w io.Writer) error {
 		return fmt.Errorf("fail to send response to remote, size mismatch")
 	}
 	return nil
+}
+
+func parseLine(line string) (mode fs.FileMode, size int64, name string, err error) {
+	words := strings.Fields(strings.TrimSuffix(line, "\n"))
+	if len(words) < 3 {
+		return 0, 0, "", fmt.Errorf("incorrect scp command param number: %d", len(words))
+	}
+
+	modeN, err := strconv.ParseUint(words[0][1:], 0, 32)
+	if err != nil {
+		return 0, 0, "", fmt.Errorf("error parsing file mode; %s", err)
+	}
+	mode = fs.FileMode(modeN)
+
+	size, err = strconv.ParseInt(words[1], 10, 64)
+	if err != nil {
+		return 0, 0, "", err
+	}
+
+	name = words[2]
+
+	return
 }
