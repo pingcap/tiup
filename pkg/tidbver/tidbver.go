8e58dfdf1cad875b9a37d171f4718aad9d009677
Shiori <yshshihaoren@qq.com>
2023-08-17 08:42:44 +0000

    add comment

--- pkg/tidbver/tidbver.go
+++ pkg/tidbver/tidbver.go
@@ -130,6 +130,7 @@ func TiCDCSupportRollingUpgrade(version string) bool {
 	return semver.Compare(version, "v6.3.0") >= 0 || strings.Contains(version, "nightly")
 }
 
+// TiCDCUpgradeBeforePDTiKV return if the given version of TiCDC should upgrade TiCDC before PD and TiKV
 func TiCDCUpgradeBeforePDTiKV(version string) bool {
 	return semver.Compare(version, "v5.1.0") >= 0 || strings.Contains(version, "nightly")
 }
