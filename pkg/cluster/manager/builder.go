3331b270fc41653d6a745ccddc7edbc4ac87e214
xhe <xw897002528@gmail.com>
2024-02-04 13:29:22 +0800

    cert renew
    Signed-off-by: xhe <xw897002528@gmail.com>

--- pkg/cluster/manager/builder.go
+++ pkg/cluster/manager/builder.go
@@ -867,6 +867,16 @@ func genTiProxySessionCerts(dir string) error {
 	}), "")
 }
 
+func delTiProxySessionCerts(dir string) error {
+	if err := os.Remove(filepath.Join(dir, "tiproxy-session.crt")); err == nil {
+		return err
+	}
+	if err := os.Remove(filepath.Join(dir, "tiproxy-session.key")); err == nil {
+		return err
+	}
+	return nil
+}
+
 // buildCertificateTasks generates certificate for instance and transfers it to the server
 func buildCertificateTasks(
 	m *Manager,
@@ -936,5 +946,13 @@ func buildCertificateTasks(
 			certificateTasks = append(certificateTasks, t)
 		}
 	})
+
+	if hasTiProxy {
+		// delete it, so tiup will generate a new cert next time to renew
+		if err := delTiProxySessionCerts(m.specManager.Path(name, spec.TempConfigPath)); err != nil {
+			return certificateTasks, err
+		}
+	}
+
 	return certificateTasks, iterErr
 }
