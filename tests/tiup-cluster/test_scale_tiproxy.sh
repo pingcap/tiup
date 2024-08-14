b20d9bcff720145b5489ab552ab9ca923f22a5fb
djshow832 <zhangming@pingcap.com>
2024-08-14 18:22:48 +0800

    try another version

--- tests/tiup-cluster/test_scale_tiproxy.sh
+++ tests/tiup-cluster/test_scale_tiproxy.sh
@@ -4,5 +4,5 @@ set -eu
 
 source script/scale_tiproxy.sh
 
-echo "test scaling of tidb and tiproxy in cluster for version v8.1.0, via easy ssh"
-scale_tiproxy v8.1.0 false false
+echo "test scaling of tidb and tiproxy in cluster for version v8.2.0, via easy ssh"
+scale_tiproxy v8.2.0 false false
