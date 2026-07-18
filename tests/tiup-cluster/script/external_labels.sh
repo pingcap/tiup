6474892d7a5419f6fb29e71474c79598396d1836
Rossi Sun <zanmato1984@gmail.com>
2026-07-18 16:41:44 -0700

    tests: polish external labels integration script

--- tests/tiup-cluster/script/external_labels.sh
+++ tests/tiup-cluster/script/external_labels.sh
@@ -11,6 +11,7 @@ function external_labels() {
     invalid_topo=./topo/external_labels_invalid.yaml
     prometheus_config=/home/tidb/deploy/prometheus-9090/conf/prometheus.yml
 
+    echo "check invalid external_labels topology"
     set +e
     invalid_output=$(tiup-cluster check $invalid_topo -i ~/.ssh/id_rsa 2>&1)
     invalid_status=$?
@@ -21,16 +22,23 @@ function external_labels() {
     fi
     echo "$invalid_output" | grep "contains reserved label 'cluster'"
 
+    echo "deploy cluster with external_labels"
     tiup-cluster --yes deploy $name $version $topo -i ~/.ssh/id_rsa
+    tiup-cluster list | grep "$name"
+
+    echo "start cluster"
     tiup-cluster --yes start $name
 
     tiup-cluster _test $name writable
+    tiup-cluster display $name
 
+    # check the generated Prometheus config after the initial deploy
     tiup-cluster exec $name -N n1 --command "grep -q \"cluster: '$name'\" $prometheus_config"
     tiup-cluster exec $name -N n1 --command "grep -q 'monitor: \"prometheus\"' $prometheus_config"
     tiup-cluster exec $name -N n1 --command "grep -q 'environment: \"production\"' $prometheus_config"
     tiup-cluster exec $name -N n1 --command "grep -q 'region: \"us-east-1\"' $prometheus_config"
 
+    echo "edit config and reload cluster"
     EDITOR=ex tiup-cluster edit-config -y $name <<EOEX
 :%s/production/staging/g
 :%s/us-east-1/eu-central-1/g
@@ -40,6 +48,7 @@ EOEX
 
     tiup-cluster _test $name writable
 
+    # verify reload updates the rendered external_labels instead of keeping the old values
     tiup-cluster exec $name -N n1 --command "grep -q \"cluster: '$name'\" $prometheus_config"
     tiup-cluster exec $name -N n1 --command "grep -q 'monitor: \"prometheus\"' $prometheus_config"
     ! tiup-cluster exec $name -N n1 --command "grep -q 'environment: \"production\"' $prometheus_config"
@@ -47,5 +56,6 @@ EOEX
     ! tiup-cluster exec $name -N n1 --command "grep -q 'region: \"us-east-1\"' $prometheus_config"
     tiup-cluster exec $name -N n1 --command "grep -q 'region: \"eu-central-1\"' $prometheus_config"
 
+    echo "destroy cluster"
     tiup-cluster --yes destroy $name
 }
