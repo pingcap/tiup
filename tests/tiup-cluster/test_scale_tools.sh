b37ed865c83fc5484e2a05029081a1634c3d920d
Ziqian Qin <eke@fastmail.com>
2026-09-07 18:21:37 +0800

    ci: bump Bookworm scale tests off TiDB v4.0.12
    TiFlash v4.0.12 cannot load on glibc 2.36 (GLIBC_PRIVATE in libpthread).
    The v4.0.12 TLS start path also hits empty-CA transfers more readily.
    Use versions already green on this PR: v6.2.0 for tools, v6.0.0 for core TLS.
    
    Signed-off-by: Ziqian Qin <eke@fastmail.com>

--- tests/tiup-cluster/test_scale_tools.sh
+++ tests/tiup-cluster/test_scale_tools.sh
@@ -4,5 +4,5 @@ set -eu
 
 source script/scale_tools.sh
 
-echo "test scaling of tools components in cluster for version v4.0.12, via easy ssh"
-scale_tools v4.0.12 false false
+echo "test scaling of tools components in cluster for version v6.2.0, via easy ssh"
+scale_tools v6.2.0 false false
